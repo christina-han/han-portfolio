4ea5ec143e6e1b6c4900f5d8c96359411cd3cf20
Christina-han <chris50719@gmail.com>
2021-05-05 14:50:51 +0800

    contact-form

--- js/styleSwitcher.js
+++ js/styleSwitcher.js
@@ -2,16 +2,18 @@
 const links = document.querySelectorAll(".alternate-style"),
       totalLinks = links.length ;
 
+
 function setActiveStyle(color) {
     for(let i=0; i<totalLinks; i++){
         if(color === links[i].getAttribute("title")){
-            links[i].removeAttribute("disabled");
+            links[i].removeAttribute("disabled");    
         }
         else{
             links[i].setAttribute("disabled", "true");
         }
     }
 }
+
 
 // Body skin
 // const bodySkin = document.querySelectorAll(".body-skin"),
